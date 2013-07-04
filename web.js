8f4cdb9e5cb53af629827f3f08d5838f2f123fd2
Dave Lowensohn <dave@handwovenpdx.com>
2013-07-04 23:29:44 +0000

    Added fs.readFileSync, no buffer trans yet.

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+var bufferstuff = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(bufferstuff);
 });
 
 var port = process.env.PORT || 5000;
